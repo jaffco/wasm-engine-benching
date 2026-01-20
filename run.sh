f36f1e0922cfd21d68a3c0d1ff47feac03c087f7
Joel A. Jaffe <joel@jaffesd.com>
2026-01-20 07:54:25 -0800

    Have `run.sh` launch after successful build on MacOS

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
+set -e
+
 cd build
 
 cmake ..
 
 cmake --build . --parallel
+
+if [ "$(uname)" = "Darwin" ]; then
+  cd ./KICKSTART_artefacts/Standalone/KICKSTART.app/Contents/MacOS
+fi
+
+./KICKSTART
